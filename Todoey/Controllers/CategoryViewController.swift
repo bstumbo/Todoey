8dfc60c8b1028c83d13b577a0069986515d75e62
bstumbo <bstumbo1@gmail.com>
2018-02-21 20:57:45 -0600

    Incorporate Chameleon library Add background colors and gradient code based on category/item number

--- Todoey/Controllers/CategoryViewController.swift
+++ Todoey/Controllers/CategoryViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import RealmSwift
+import ChameleonFramework
 
 class CategoryViewController: SwipeTableViewController {
     
@@ -21,8 +22,8 @@ class CategoryViewController: SwipeTableViewController {
 
         loadCategories()
         
-    
-        
+        tableView.separatorStyle = .none
+
     }
     
     //MARK: - TableView Datasource Methods
@@ -36,6 +37,8 @@ class CategoryViewController: SwipeTableViewController {
         let cell = super.tableView(tableView, cellForRowAt: indexPath)
         
         cell.textLabel?.text = categories?[indexPath.row].name ?? "No Categories Added Yet"
+        
+        cell.backgroundColor = UIColor(hexString: (categories?[indexPath.row].color ?? "1D98F6"))
  
         return cell
     }
@@ -105,6 +108,7 @@ class CategoryViewController: SwipeTableViewController {
             
             let newCategory = Category()
             newCategory.name = textField.text!
+            newCategory.color = UIColor.randomFlat.hexValue()
          
             self.save(category: newCategory)
             
--- Todoey/Controllers/TodoListViewController.swift
+++ Todoey/Controllers/TodoListViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import RealmSwift
+import ChameleonFramework
 
 class TodoListViewController: SwipeTableViewController {
     
@@ -24,6 +25,7 @@ class TodoListViewController: SwipeTableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        tableView.separatorStyle = .none
         
         }
     
@@ -41,6 +43,11 @@ class TodoListViewController: SwipeTableViewController {
         if let item = todoItems?[indexPath.row] {
             cell.textLabel?.text = item.title
             
+            if let color = UIColor(hexString: selectedCategory!.color)?.darken(byPercentage:CGFloat(indexPath.row) / CGFloat(todoItems!.count)) {
+                cell.backgroundColor = color
+                cell.textLabel?.textColor = ContrastColorOf(color, returnFlat: true)
+            }
+
             //Ternary operator ==>
             // value = conditon ? valuIfTrue : valueIfFalse
             
